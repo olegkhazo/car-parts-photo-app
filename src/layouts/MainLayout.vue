1f1776298dc370f29ea784246a11b1c8388857cd
Oleg Zhuravko <o.zhuravko@gmail.com>
2024-09-28 16:36:25 +0200

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>Imperial Auto</q-toolbar-title>
+
+        <div>Imperial Auto Photo APP</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Some menu list </q-item-label>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,130 @@
+<template>
+  <q-page class="flex flex-center">
+    <div v-if="!cameraActive">
+      <q-btn label="Begin" @click="startCamera" />
+    </div>
+
+    <div v-else class="camera-view">
+      <q-btn
+        label="DONE"
+        flat
+        @click="stopCamera"
+        style="
+          position: absolute;
+          top: 10px;
+          right: 10px;
+          background-color: #dcdad2a6;
+          z-index: 10;
+          opacity: 0.5;
+        "
+      />
+      <video ref="videoElement" autoplay playsinline></video>
+
+      <q-btn
+        icon="photo_camera"
+        flat
+        @click="takePhoto"
+        style="
+          position: absolute;
+          bottom: 10px;
+          left: 50%;
+          transform: translateX(-50%);
+          background: #dcdad2a6;
+          padding: 5px 7px;
+          border-radius: 50%;
+          opacity: 0.4;
+        "
+      />
+
+      <canvas ref="canvasElement" style="display: none"></canvas>
+    </div>
+
+    <div v-if="photoCollection.length > 0" class="photoCollection-gallery">
+      <div
+        v-for="(photo, index) in photoCollection"
+        :key="index"
+        class="photo-item"
+      >
+        <img :src="photo" />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { ref, onBeforeUnmount } from "vue";
+
+const videoElement = ref(null);
+const canvasElement = ref(null);
+const cameraActive = ref(false);
+const photoCollection = ref([]);
+
+const startCamera = async () => {
+  cameraActive.value = true;
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    videoElement.value.srcObject = stream;
+  } catch (error) {
+    console.error("Error accessing the camera:", error);
+  }
+};
+
+const stopCamera = () => {
+  const stream = videoElement.value.srcObject;
+  const tracks = stream.getTracks();
+  tracks.forEach((track) => track.stop());
+  cameraActive.value = false;
+};
+
+const takePhoto = () => {
+  const context = canvasElement.value.getContext("2d");
+
+  canvasElement.value.width = videoElement.value.videoWidth;
+  canvasElement.value.height = videoElement.value.videoHeight;
+  context.drawImage(videoElement.value, 0, 0);
+
+  const photoData = canvasElement.value.toDataURL("image/png");
+  photoCollection.value.push(photoData);
+
+  setTimeout(() => {
+    // For now it's a pause but you can do another action when photo action
+  }, 5000);
+};
+
+onBeforeUnmount(() => {
+  if (cameraActive.value) {
+    stopCamera();
+  }
+});
+</script>
+
+<style scoped>
+.camera-view {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+}
+
+video {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.photoCollection-gallery {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.photo-item {
+  margin: 10px;
+}
+
+img {
+  max-width: 100px;
+  max-height: 100px;
+  object-fit: cover;
+}
+</style>
